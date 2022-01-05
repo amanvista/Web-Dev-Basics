2b2d4026d46e00026d98acdf89e1fe7a4cbb1fa7
Aman Bisht <amanvista@gmail.com>
2022-01-05 11:01:40 +0530

    working

---
+++ codingcraft/src/components/NewExpenses/NewExpense.js
@@ -0,0 +1,11 @@
+import './NewExpense.css'
+import React from "react"
+import ExpenseForm from './ExpenseForm'
+
+const NewExpense = () => {
+    <div class="new-expense">
+        <ExpenseForm/>
+    </div>
+}
+
+export default NewExpense
